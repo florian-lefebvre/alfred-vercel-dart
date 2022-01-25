1b7efbcb13ad1fa15ae0ccda39965eee998d17f7
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-01-25 19:59:43 +0100

    use documentation example

--- api/index.dart
+++ api/index.dart
@@ -1,24 +1,7 @@
-import 'dart:io';
+import 'dart:io' show Platform;
+import 'package:shelf/shelf.dart';
 
-import 'package:alfred/alfred.dart';
-
-Future handler(req) async {
-  final app = Alfred();
-
-  app.get('/text', (req, res) => 'Text response');
-
-  app.get('/json', (req, res) => {'json_response': true});
-
-  app.get('/jsonExpressStyle', (req, res) {
-    res.json({'type': 'traditional_json_response'});
-  });
-
-  app.get('/file', (req, res) => File('test/files/image.jpg'));
-
-  app.get('/html', (req, res) {
-    res.headers.contentType = ContentType.html;
-    return '<html><body><h1>Test HTML</h1></body></html>';
-  });
-
-  await app.listen(6565); //Listening on port 6565
+Response handler(Request req) {
+  final version = Platform.version.split(" ").first;
+  return Response.ok('Hello, from Dart v$version!');
 }
