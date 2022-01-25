4177ab91dc5aa8bbed71c397525f78ffb85e66de
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-01-25 20:23:55 +0100

    implement basic router

--- api/index.dart
+++ api/index.dart
@@ -1,7 +1,20 @@
 import 'dart:io' show Platform;
 import 'package:shelf/shelf.dart';
 
-Response handler(Request req) {
-  final version = Platform.version.split(" ").first;
-  return Response.ok('Hello, from Dart v$version! ${req.requestedUri.path}');
+Future<Response> handler(Request req) async {
+  return await Router.handle(req, req.requestedUri.path);
+}
+
+class Router {
+  const Router._();
+
+  static Future<Response> handle(Request req, String url) async {
+    switch (url) {
+      case "/":
+        final version = Platform.version.split(" ").first;
+        return Response.ok('Hello, from Dart v$version! ${req.requestedUri.path}');
+      default:
+        return Response.notFound("Page $url not found");
+    }
+  }
 }
