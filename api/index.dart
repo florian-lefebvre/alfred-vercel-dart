eead56cf181c12054c4b68fca4b831eeb19497a7
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-01-25 20:26:31 +0100

    add json and html routes

--- api/index.dart
+++ api/index.dart
@@ -12,7 +12,12 @@ class Router {
     switch (url) {
       case "/":
         final version = Platform.version.split(" ").first;
-        return Response.ok('Hello, from Dart v$version! ${req.requestedUri.path}');
+        return Response.ok('Hello, from Dart v$version!');
+      case "/json":
+        return Response.ok(<String, dynamic>{"key": "value", "test": 3}, headers: {"content-type": "application/json"});
+      case "/html":
+        return Response.ok("<html><body><h1>Hello, from Dart!</h1></body></html>",
+            headers: {"content-type": "text/html"});
       default:
         return Response.notFound("Page $url not found");
     }
